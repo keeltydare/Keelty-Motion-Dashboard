4ef57ac266519887bf6f85219c181c231b9c5a7e
Keelty <keeltydare@gmail.com>
2021-05-31 23:30:06 -0400

    motion

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -36,7 +36,8 @@ ready(() => {
     //*********** frameTL init ****************
     gsap.set(["#speed-petals-frame", "#speed-screen-frame", "#fuel-frame-petals", "#fuel-screen-frame", "#rpm-petals-frame", "#rpm-screen-frame", "#temp-bubble-frame", "#weather-frame", "#music-frame"], {drawSVG:false, alpha:0});
     gsap.set(["#speed-petals-frame", "#fuel-frame-petals", "#rpm-petals-frame"], {transformOrigin:"center center", scale:0});
-    gsap.set(["#mph", "#speed-petals", "#speed-screen", "#mph-number", "#gas-tank", "#fuel-petals", "#fuel-screen", "#full-frame", "#empty-frame", "#rpm-figure", "#rpm-petals", "#rpm-screen", "#rpm-number"], {alpha:0, transformOrigin:"center center"});
+    gsap.set(["#speed-petals", "#fuel-petals", "#rpm-petals"], {alpha:0});
+    gsap.set(["#fuel-1", "#fuel-2","#fuel-3","#fuel-4","#fuel-5","#fuel-6","#fuel-7","#fuel-8"], {scale:0,  transformOrigin:"center center"})
     gsap.set(["#promiscuous", "#glamorous", "#beautiful-girls", "#barbie-girl"], {transformOrigin:"center right", scale:0});
     gsap.set(["#P", "#R", "#N", "#D", "#L"], {alpha:0, transformOrigin:"center center"});
     gsap.set( "#selection-button", {scale:0, transformOrigin:"center center"});
@@ -46,12 +47,11 @@ ready(() => {
     gsap.set(["#cloudy", "#cloud", "#temp", "#music-controls"], {scale:0, transformOrigin:"center center"});
     gsap.set(["#date", "#time"], {scale:0});
 
-    gsap.set("#odometer", {display:"none"});
-    gsap.set("#call-ken-button", {display:"none"});
-    gsap.set("#gps-button", {display:"none"});
-
     //*********** detailsTL init ****************
-    
+    gsap.set("#odometer-frame", {drawSVG:false, alpha:0});
+    gsap.set(["#mi", "#miles", "#gps-frame", "#gps", "#phone-frame", "#call-ken", "#full-frame", "#empty-frame", "#mph", "#rpm-number", "#mph-number",  "#gas-tank", "#rpm-figure"], {scale:0, transformOrigin:"center center"});
+    gsap.set(["#phone", "#gps-bubble", "#rpm-screen", "#speed-screen", "#fuel-screen"], {alpha:0});
+   
     //*********** motionTL init ****************
 
 
@@ -109,12 +109,12 @@ ready(() => {
   //*********** pinkFillTL ****************
   function pinkFillTL(){
     let tl = gsap.timeline();
-    tl.to("#weather-bubble", {alpha:1, duration:0.5})
-    .to(["#cloud", "#cloudy"], {scale:1, duration:0.5})
-    .to("#music-bubble", {alpha:1, duration:0.5}, "-=0.5")
-    .to("#music-controls", {scale:1, duration:0.5})
-    .to("#temp-bubble", {alpha:1, duration:0.5}, "-=0.5")
-    .to(["#temp"], {scale:1, duration:0.5})
+    tl.to("#weather-bubble", {alpha:1, duration:1})
+    .to(["#cloud", "#cloudy"], {scale:1, duration:0.5}, "-=0.5")
+    .to("#music-bubble", {alpha:1, duration:1}, "-=0.5")
+    .to("#music-controls", {scale:1, duration:0.5}, "-=0.5")
+    .to("#temp-bubble", {alpha:1, duration:1}, "-=0.5")
+    .to(["#temp"], {scale:1, duration:0.5}, "-=0.5")
     .from(["#date", "#time"], {y:900, alpha:1}, "-=1")
     .to(["#date", "#time"], {scale:1, ease:"back.out"}, "-=1")
     ;
@@ -125,14 +125,38 @@ ready(() => {
   //*********** detailsTL ****************
   function detailsTL(){
     let tl = gsap.timeline();
-
+    tl.to("#odometer-frame", {alpha:1, drawSVG:true, duration:1})
+    .to(["#miles", "#mi"], {scale:1, duration:0.5}, "-=0.5")
+    .to(["#phone", "#gps-bubble"], {alpha:1, duration:1}, "-=0.5")
+    .to(["#call-ken", "#gps"], {scale:1, duration:0.5}, "-=0.5")
+    .to("#glamorous", {scale:1, duration:1}, "-=0.5")
+    .to(["#fuel-screen", "#speed-screen", "#rpm-screen"], {alpha:1, duration:0.5})
+    .to(["#rpm-figure", "#gas-tank", "#mph", "#full-frame", "#empty-frame"], {scale:1, duration:1})
+    .to(["#mph-number", "#rpm-number"], {scale:1, duration:0.5})
+    ;
     return tl;
   }
 
   //*********** motionTL ****************
   function motionTL(){
     let tl = gsap.timeline();
-
+    tl.to("#fuel-petals", {alpha:1})
+    .to("#fuel-1", {scale:1, duration:0.5, ease:"power1.out"})
+    .to("#fuel-2", {scale:1, duration:0.5, ease:"power1.out"})
+    .to("#fuel-3", {scale:1, duration:0.5, ease:"power1.out"})
+    .to("#fuel-4", {scale:1, duration:0.5, ease:"power1.out"})
+    .to("#fuel-5", {scale:1, duration:0.5, ease:"power1.out"})
+    .to("#f-forward", {transformOrigin:"center center", alpha:0.8, fill:"#63e3fa", yoyo:true, duration:1, repeat:1}, "glamorous")
+    .to("#glamorous", {transformOrigin:"center left", scale:0, duration:0.5}, "glamorous")
+    .to("#promiscuous", {scale:1, duration:0.5}, "glamorous+=0.5")
+    .to("#f-forward", {transformOrigin:"center center", alpha:0.8, fill:"#63e3fa", yoyo:true, duration:1, repeat:1}, "beautifulGirls")
+    .to("#promiscuous", {transformOrigin:"center left", scale:0, duration:0.5}, "beautifulGirls")
+    .to("#beautiful-girls", {scale:1, duration:0.5}, "beautifulGirls+=0.5")
+    .to("#f-forward", {transformOrigin:"center center", alpha:0.8, fill:"#63e3fa", yoyo:true, duration:1, repeat:1}, "barbieGirl")
+    .to("#beautiful-girls", {transformOrigin:"center left", scale:0, duration:0.5}, "barbieGirl")
+    .to("#barbie-girl", {scale:1, duration:0.5}, "barbieGirl+=0.5")
+    
+    ;
     return tl;
   }
 
@@ -150,10 +174,10 @@ ready(() => {
 
 
 mainTL.add(logoTL())
-.add(frameTL(), "-=1")
-.add(pinkFillTL())
-.add(detailsTL())
-.add(motionTL())
+.add(frameTL(), "-=2")
+.add(pinkFillTL(), "-=1")
+.add(detailsTL(), "-=1")
+.add(motionTL(), "-=0.5")
 
 ;//tl END
  
